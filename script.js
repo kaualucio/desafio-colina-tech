5dce043bed2352ea3167e15b57d5bb4bb061b299
Your Name <you@example.com>
2024-07-16 11:01:23 -0300

    finish reponsiviness

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const allSections = document.querySelectorAll('section')
 const navLinks = document.querySelectorAll('header nav ul a')
+const menuHamburguer = document.querySelector('button#menu-hamburguer')
+const menuMobile = document.querySelector('div.mobile-nav > nav')
 
+let isOpen = !!menuHamburguer.getAttribute('value')
 
 navLinks.forEach(element => {
   element.addEventListener('click',() => {
@@ -10,3 +13,16 @@ navLinks.forEach(element => {
     element.classList.add('active')
   })
 }); 
+
+menuHamburguer.addEventListener('click', () => {
+  if(isOpen) {
+    menuMobile.classList.remove('hidden')
+    menuMobile.classList.add('flex')
+    isOpen = false
+  } else {
+    menuMobile.classList.remove('flex')
+    menuMobile.classList.add('hidden')
+    isOpen = true
+  }
+})
+
